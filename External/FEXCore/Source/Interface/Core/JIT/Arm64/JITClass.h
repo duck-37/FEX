fc17f847cf865efcfb273319baaf1df4fcfc1911
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-10-27 10:38:34 +0200

    partial arm64 support

--- External/FEXCore/Source/Interface/Core/JIT/Arm64/JITClass.h
+++ External/FEXCore/Source/Interface/Core/JIT/Arm64/JITClass.h
@@ -206,8 +206,11 @@ private:
    * @name Dispatch Helper functions
    * @{ */
   aarch64::Label LoopTop{};
+  aarch64::Label FullLookup{};
   aarch64::Label Exit{};
   uint64_t AbsoluteLoopTopAddress{};
+  uint64_t AbsoluteFullLookupAddress{};
+  uint64_t InterpreterFallbackHelperAddress{};
   uint64_t ThreadPauseHandlerAddress{};
 
   Label ThreadPauseHandler{};
