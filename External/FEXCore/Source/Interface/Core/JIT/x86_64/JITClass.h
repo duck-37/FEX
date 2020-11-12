8ccbe0904c6d4a4d1cf8d869df77cad2fabfc770
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-11-12 17:21:06 +0200

    inline const x86 support

--- External/FEXCore/Source/Interface/Core/JIT/x86_64/JITClass.h
+++ External/FEXCore/Source/Interface/Core/JIT/x86_64/JITClass.h
@@ -134,6 +134,8 @@ private:
     CurrentCodeBuffer = &CodeBuffers.emplace_back(Buffer);
   }
 
+  static uint64_t ExitFunctionLink(JITCore* code, FEXCore::Core::InternalThreadState *Thread, uint64_t *record);
+
   // This is the initial code buffer that we will fall back to
   // In a program without signals and code clearing, we will typically
   // only have this code buffer
@@ -150,6 +152,7 @@ private:
 
   uint64_t AbsoluteLoopTopAddress{};
   uint64_t AbsoluteFullLookupAddress{};
+  uint64_t ExitFunctionLinkerAddress{};
   uint64_t ThreadStopHandlerAddress{};
   uint64_t ThreadPauseHandlerAddress{};
 
